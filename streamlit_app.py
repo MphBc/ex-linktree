01a6001a8fc2938d624400ddc8c89831c9844e12
MphBc <87320194+MphBc@users.noreply.github.com>
2022-09-08 15:40:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from PIL import Image
 
 load_css()
 
+
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('meb_pic.jpg'))
 
@@ -13,6 +14,8 @@ st.info('🥣 คอร์นเฟลกคาราเมล 🥣  🍪ซอ�
 
 icon_size = 20
 
-st_button('twitter', 'https://twitter.com/meb_shoppp?s=20&t=fmyjrA_YPXAMsFqjpyFYqQ', 'Follow me on Twitter', icon_size)
-st_button('facebook', 'https://www.facebook.com/meb.shoppp/', 'Follow me on Facebook', icon_size)
-st_button('instagram', 'https://www.instagram.com/meb.bakery/?igshid=NmNmNjAwNzg%3D&fbclid=IwAR3BQZPlqtnmStk7eKMiiP80ef4wDbWYfXEyxjbt8tV9hLzY-T2x328d9FA', 'Follow me on Instagram', icon_size)
+st_button('twitter', 'https://twitter.com/meb_shoppp?s=20&t=fmyjrA_YPXAMsFqjpyFYqQ', 'Twitter', icon_size)
+st_button('facebook', 'https://www.facebook.com/meb.shoppp/', 'Facebook', icon_size)
+st_button('instagram', 'https://www.instagram.com/meb.bakery/?igshid=NmNmNjAwNzg%3D&fbclid=IwAR3BQZPlqtnmStk7eKMiiP80ef4wDbWYfXEyxjbt8tV9hLzY-T2x328d9FA', 'Instagram Bakery', icon_size)
+st_button('instagram', 'https://www.instagram.com/meb_shoppp/?igshid=NmNmNjAwNzg%3D&fbclid=IwAR0PwP_ns38vNHekGW1hXSNreHRARH9k2fYoxVvE0ZFwjAfwWyqcXDFeG1k', 'Instagram Meb shoppp', icon_size)
+st_button('line', 'https://line.me/R/ti/p/@638itmws', 'Line official', icon_size)
